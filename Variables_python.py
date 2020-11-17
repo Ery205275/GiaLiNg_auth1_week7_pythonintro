ff27cd48a95faa69a9d5707009deb532af84a43b
Nguyen Gia Linh <nguyengialinh@Nguyens-MacBook-Pro.local>
2020-11-17 14:36:04 -0500

    updating python

---
+++ Variables_python.py
@@ -0,0 +1,24 @@
+#this is comment
+print("This is my first python")
+
+name = "Gia Linh"
+age = "old"
+eyecolour = "brown"
+haircolour = "brown"
+
+print("Name is currently" + name)
+
+name = input("What is your name?")
+age = 19
+
+print("Your name is now:" + name + "and your age is" + str(age))
+
+counter = 0
+
+while(counter < 8):
+	counter += 1
+	print ("counter is now at:" + counter)
+
+print("counter is greater than 8, our loop is done")
+
+
